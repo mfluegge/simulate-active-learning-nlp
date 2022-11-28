f9d347ad59a4909e96d1a06897b4c37179c94095
mfluegge <maflueg@gmail.com>
2022-11-28 12:47:06 +0100

    add setfit implementation

--- src/strategies.py
+++ src/strategies.py
@@ -6,6 +6,10 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.cluster import KMeans
+from sentence_transformers import SentenceTransformer, InputExample, losses
+from torch.utils.data import DataLoader
+from copy import deepcopy
+
 
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.DEBUG)
 
@@ -351,3 +355,103 @@ class USELogisticRegressionLeastConfidentDiverseKMeansStrategy(LabelingStrategy)
         pred = self.latest_clf.predict(self.eval_text_embeddings)
 
         return pred
+
+class SetFitRandomStrategy(LabelingStrategy):
+    def __init__(self, texts, label_per_iteration=10, sent_trf=None):
+        super().__init__(texts, label_per_iteration)
+
+        if sent_trf is None:
+            logging.info("Loading SentenceTransformer")
+            self.sent_trf = SentenceTransformer("sentence-transformers/all-MiniLM-L6-v2")
+        else:
+            self.sent_trf = sent_trf 
+
+        self.labeled_indices = []
+        self.labels = []
+        self.latest_clf = None
+        
+        self.text_embeddings = None
+        self.eval_text_embeddings = None
+
+    def get_next_examples(self):
+        unlabeled_indices = np.setdiff1d(
+            np.arange(len(self.texts)), self.labeled_indices
+        )
+
+        if len(unlabeled_indices) <= self.label_per_iteration:
+            return unlabeled_indices
+
+        next_indices = np.random.choice(
+            unlabeled_indices,
+            size=self.label_per_iteration, 
+            replace=False
+        )
+
+        return next_indices
+    
+    def add_labels(self, indices, labels):
+        self.labeled_indices += indices
+        self.labels += labels
+    
+    def predict(self, eval_texts):
+        logging.debug("Training SetFit model with labeled examples for prediction")
+        logging.debug("Cloning sentence transformer model")
+        model = deepcopy(self.sent_trf)
+
+        logging.debug("Generating training examples for SetFit model")
+        train_texts = [self.texts[i] for i in self.labeled_indices]
+        input_examples = _make_setfit_examples(train_texts, np.array(self.labels), R=20)
+
+        train_dataloader = DataLoader(
+            input_examples,
+            shuffle=True, 
+            batch_size=16
+        )
+
+        train_loss = losses.CosineSimilarityLoss(model)
+
+        logging.debug("Fitting sentence transformer")
+        model.fit(
+            train_objectives=[(train_dataloader, train_loss)],
+            epochs=1,
+            optimizer_params={"lr": 1e-3},
+            warmup_steps=int(len(train_loader) * 0.1),
+        )
+
+        model.eval()
+
+        logging.debug("Encoding labeled sentences")
+        train_embs = model.encode(train_texts)
+
+        logging.debug("Training classification head")
+        clf = LogisticRegression(random_state=42).fit(train_embs, self.labels)
+
+        logging.debug("Encoding validation texts")
+        eval_embs = model.encode(eval_texts)
+
+        logging.debug("Predicting validation texts")
+        pred = clf.predict(eval_embs)
+
+        return pred
+
+
+def _make_setfit_examples(texts, labels, R=20):
+    # hard to decipher from the paper, but looking at the code it becomes a little clearer
+    # R likely means the number of labeling iterations
+    # and in each labeling iteration, you generate 1 positive and 1 negative example per labeled sentence
+    # so you have 2 * R * len(texts) training examples for contrastive training
+    training_examples = []
+    for class_label in np.unique(labels):
+        class_texts = [texts[i] for i in np.where(labels == class_label)[0]]
+        outside_class_texts = [texts[i] for i in np.where(labels != class_label)[0]]
+
+        for text_in_class_ix, text in enumerate(class_texts):
+            other_in_class_texts = [t for i, t in enumerate(class_texts) if i != text_in_class_ix]
+            
+            positives = np.random.choice(other_in_class_texts, size=R, replace=len(other_in_class_texts) < R)
+            training_examples += [InputExample(text, other_text, label=1.0) in positives]
+        
+            negatives = np.random.choice(outside_class_texts, size=R, replace=len(outside_class_texts) < R)
+            training_examples += [InputExample(text, other_text, label=0.0) in negatives]
+
+    return training_examples
